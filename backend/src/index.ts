0d2cfb386f893be0a98fc26df73a366d633285f4
“[Akshit <akshitlakhera14@gmail.com>
2024-02-28 00:06:37 +0530

    Hashing of password is doen

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -10,17 +10,17 @@ const app = new Hono<{
 
 //Always try to initialize as many thing in inside not in global context as it could start worker on specific route and you coould loose data
 // Routing
-
 app.post('/api/v1/signup',async (c) => {
   const prisma = new PrismaClient({
     datasourceUrl: c.env.DATABASE_URL,
 }).$extends(withAccelerate())
 const body = await c.req.json();
 try {
+  const hashedPassword = await hashPassword(body.password)
   const user =await prisma.user.create({
     data : {
       email:body.email,
-      password:body.password
+      password:hashedPassword
     }
   })
   return c.text("jwt here")
@@ -28,6 +28,20 @@ try {
   return c.status(403)
 }
 })
+
+// Hashing password logic
+async function hashPassword(password:string): Promise<string> {
+    // Convert the password string to a Uint8Array buffer
+    const encoder = new TextEncoder();
+    const data = encoder.encode(password);
+
+    // Hash the password using SHA-256 Algorithm
+    const hashBuffer = await crypto.subtle.digest('SHA-256', data);
+    // convert this hasbuffer into hexadecimal(readable string)
+    const hashArray = Array.from(new Uint8Array(hashBuffer));
+    const hashHex  =  hashArray.map(byte => byte.toString(16).padStart(2,'0')).join('');;
+    return hashHex
+}
 
 app.post('/api/v1/signin',(c) => {
   return c.text('Hello Hono!')
