9526b8dad322305fc891153906a630631cffc90b
“[Akshit <akshitlakhera14@gmail.com>
2024-02-27 23:47:05 +0530

    signup code added

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,34 @@
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
 import { Hono } from 'hono'
+// This is a way to define type in typescript with hono
+const app = new Hono<{
+	Bindings: {
+		DATABASE_URL: string
+	}
+}>();
 
-const app = new Hono()
-
+//Always try to initialize as many thing in inside not in global context as it could start worker on specific route and you coould loose data
 // Routing
-app.post('/api/v1/signup',(c) => {
-  return c.text('Hello Hono!')
+
+app.post('/api/v1/signup',async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+}).$extends(withAccelerate())
+const body = await c.req.json();
+try {
+  const user =await prisma.user.create({
+    data : {
+      email:body.email,
+      password:body.password
+    }
+  })
+  return c.text("jwt here")
+}catch(e) {
+  return c.status(403)
+}
 })
+
 app.post('/api/v1/signin',(c) => {
   return c.text('Hello Hono!')
 })
@@ -14,11 +37,11 @@ app.post('/api/v1/blog',(c) => {
   return c.text('Hello Hono!')
 })
 
-app.post('/api/v1/blog',(c) => {
+app.put('/api/v1/blog',(c) => {
   return c.text('Hello Hono!')
 })
 
-app.post('/api/v1/blog/:id',(c) => {
+app.get('/api/v1/blog/:id',(c) => {
   return c.text('Hello Hono!')
 })
 
