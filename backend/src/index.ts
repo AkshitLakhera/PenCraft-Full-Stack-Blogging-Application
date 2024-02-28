7b6025e8851dc58105e9804edd6703b77ad3b8ae
“[Akshit <akshitlakhera14@gmail.com>
2024-02-28 21:53:06 +0530

    useRouter code added in user.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,6 +3,7 @@ import { withAccelerate } from '@prisma/extension-accelerate'
 import { Hono } from 'hono'
 import { use } from 'hono/jsx';
 import { decode, sign, verify } from 'hono/jwt'
+import { userRouter } from './routes/user';
 // This is a way to define type in typescript with hono
 
 // This is how we give type to variables in typescript
@@ -16,6 +17,7 @@ const app = new Hono<{
 	}
 }>();
 // Middleware
+// The * character in the route pattern /api/v1/blog/* acts as a wildcard. 
 app.use('/api/v1/blog/*', async (c, next) => {
   // Get the header
   // verify the header
@@ -39,89 +41,10 @@ app.use('/api/v1/blog/*', async (c, next) => {
 
 //Always try to initialize as many thing in inside not in global context as it could start worker on specific route and you coould loose data
 // Routing
-
-//                                          Signup
-app.post('/api/v1/signup',async (c) => {
-  const prisma = new PrismaClient({
-    datasourceUrl: c.env.DATABASE_URL,
-}).$extends(withAccelerate())
-const body = await c.req.json();
-try {
-  const hashedPassword = await hashPassword(body.password)
-  const user =await prisma.user.create({
-    data : {
-      email:body.email,
-      password:hashedPassword
-    }
-  })
-  // Defined the jwt secret in wrangler.toml and also defiend type inside binding at hono.
-  const jwt = await sign({id:user.id},c.env. JWT_SECRET)
-  return c.json({token:jwt})
-
-}catch(e) {
-  return c.status(403)
-}
-})
-
-// Hashing password logic
-async function hashPassword(password:string): Promise<string> {
-    // Convert the password string to a Uint8Array buffer
-    const encoder = new TextEncoder();
-    const data = encoder.encode(password);
-
-    // Hash the password using SHA-256 Algorithm
-    const hashBuffer = await crypto.subtle.digest('SHA-256', data);
-    // convert this hasbuffer into hexadecimal(readable string)
-    const hashArray = Array.from(new Uint8Array(hashBuffer));
-    const hashHex  =  hashArray.map(byte => byte.toString(16).padStart(2,'0')).join('');;
-    return hashHex
-}
-//                                                 Sigin in
-
-app.post('/api/v1/signin', async (c) => {
-  try {
-    const prisma = new PrismaClient({
-      datasourceUrl: c.env.DATABASE_URL,
-    }).$extends(withAccelerate());
-
-    const body = await c.req.json();
-    const user = await prisma.user.findUnique({
-      where: {
-        email: body.email,
-      },
-    });
-
-    if (!user) {
-      // Return a 401 Unauthorized error response if the user is not found
-      // Cloudflare Workers, don't directly support chaining response methods like Express.js does.
-      c.status(401);
-      return c.json({ error: "Incorrect user found" });
-    }
-
-    // Compare the hashed password from the database with the hashed password from the request
-    const hashedPassword = await hashPassword(body.password);
-    if (hashedPassword !== user.password) {
-      // Return a 401 Unauthorized error response if the password is incorrect
-      c.status(401);
-      return c.json({ error: "Incorrect password" });
-    }
-
-    // Generate a JWT token for authentication
-    const jwt = await sign({ id: user.id }, c.env.JWT_SECRET);
-
-    // Return the JWT token as a response
-    return c.json({ token: jwt });
-  } catch (error) {
-    // Handle any errors that occur during the process
-    console.error('Error signing in:', error);
-    // Return a 500 Internal Server Error response
-    c.status(500);
-    return c.json({ error: 'Internal server error' });
-  }
-});
-
+app.route('/api/v1/user', userRouter)
 app.post('/api/v1/blog',(c) => {
-  return c.text('Hello Hono!')
+  console.log(c.get('userId'))
+  return c.text('signin route')
 })
 
 app.put('/api/v1/blog',(c) => {
