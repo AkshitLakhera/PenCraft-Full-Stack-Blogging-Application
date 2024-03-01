7ade18b22e4ddba87c6ee34821a3f62852837b03
“[Akshit <akshitlakhera14@gmail.com>
2024-03-01 22:33:51 +0530

    Added zod code index.js

---
+++ common/src/index.ts
@@ -0,0 +1,26 @@
+import z from "zod";
+export const signupInput = z.object({
+    email:z.string().email(),
+    password:z.string(),
+    name:z.string().optional(),
+
+})
+export type SignupType =  z.infer<typeof signupInput>;
+ export const siginInput = z.object({
+    email:z.string().email(),
+    password:z.string(),
+ });
+ export type SigninType = z.infer<typeof siginInput>;
+  
+ export const createPostInput = z.object({
+    title:z.string(),
+    content:z.string(),
+ });
+
+ export type CreatePostType =  z.infer<typeof createPostInput>
+
+ export const updatePost = z.object({
+    title:z.string().optional(),
+    content:z.string().optional(),
+ })
+ export type UpdatePost = z.infer<typeof updatePost>
