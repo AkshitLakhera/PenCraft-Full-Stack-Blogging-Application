c45a07159a6dd8956d964de125d72685ec1e3146
“[Akshit <akshitlakhera14@gmail.com>
2024-03-07 23:30:15 +0530

    Link added in navbar

--- frontend/src/components/Appbar.tsx
+++ frontend/src/components/Appbar.tsx
@@ -1,9 +1,14 @@
-import { Avatar } from "./BlogCard"
+import { Link } from "react-router-dom";
+import { Avatar } from "./BlogCard";
 export const Appbar = () => {
   return (
-    <div className="flex justify-between border-b p-3 m-1">
+    <Link to={"/blogs"}>
+      <div className="flex justify-between border-b p-3 m-1">
         <div className="font-bold text-lg">PenCraft</div>
-        <div><Avatar name="Akshit"/></div>
-    </div>
-  )
-}
+        <div>
+          <Avatar name="Akshit" />
+        </div>
+      </div>
+    </Link>
+  );
+};
