57d42dd5e66fdea7897010a7c1af81f801cf5f27
“[Akshit <akshitlakhera14@gmail.com>
2024-03-19 11:10:37 +0530

    Removed imageupload route

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,7 +2,6 @@ import { Hono } from 'hono'
 import { cors } from 'hono/cors'
 import { userRouter } from './routes/user';
 import { blogRouter }  from './routes/blog' ;
-import { imageRouter } from './routes/imageroute';
 // This is a way to define type in typescript with hono
 
 // This is how we give type to variables in typescript
@@ -20,5 +19,4 @@ app.use('/*', cors())
 // Routing
 app.route('/api/v1/user', userRouter);
 app.route('/api/v1/blog',blogRouter);
-app.route('/api/v1/image',imageRouter)
 export default app
