e1d30dd5481edec6b3c49ab574f132e3b6158b0c
ayushrana83 <168258223+ayushrana83@users.noreply.github.com>
2024-10-04 23:01:24 +0530

    authenticationPage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
-import { Signup } from "./pages/Signup"
-import { Signin } from "./pages/Signin"
+import { SignUpPage } from "./pages/SignUpPage.tsx"
+import { SignInPage } from "./pages/SignInPage.tsx"
 import { Blog } from "./pages/Blog"
 import { Blogs } from "./pages/Blogs"
 import { Publish } from "./pages/Publish"
@@ -16,8 +16,8 @@ function App() {
         <Routes>
           <Route path="/bookmark" element={<Bookmark />} />
           <Route path="/" element={<Landing />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/signin" element={<Signin />} />
+          <Route path="/signup" element={<SignUpPage/>} />
+          <Route path="/signin" element={<SignInPage />} />
           <Route path="/blog/:id" element={<Blog />} />
           <Route path="/blogs" element={<Blogs />} />
           <Route path="/publish" element={<Publish />} />
