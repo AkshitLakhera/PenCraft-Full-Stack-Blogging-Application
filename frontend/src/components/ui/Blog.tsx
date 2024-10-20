9fce737753c7067d37a62890182436d875bed33c
khaulanauman <khaulanauman@gmail.com>
2024-10-20 16:39:25 +0500

    removed obsolete line of code to fix build error

--- frontend/src/components/ui/Blog.tsx
+++ frontend/src/components/ui/Blog.tsx
@@ -50,7 +50,6 @@ const Blog: FC<BlogProps> = ({
               width="13px"
               version="1.1"
               viewBox="0 0 512 512"
-              style={{ enableBackground: "new 0 0 512 512" }}
               xmlns="http://www.w3.org/2000/svg"
               xmlSpace="preserve"
             >
