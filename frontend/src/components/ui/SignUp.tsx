bb47094db5eb882a6504f65543ab25abb999e479
Shariq <shariqsd2003@gmail.com>
2024-10-25 01:43:12 +0530

    Fix Conflicts

--- frontend/src/components/ui/SignUp.tsx
+++ frontend/src/components/ui/SignUp.tsx
@@ -216,19 +216,26 @@ export default function SignUp() {
               className={`${theme === `light` ? `border-2 border-gray-800` : ""} group flex transform-gpu dark:[border:1px_solid_rgba(255,255,255,.1)] dark:[box-shadow:0_-20px_80px_-20px_#8686f01f_inset]  border-white/10  items-center justify-center py-5 border rounded-lg hover:bg-transparent/50 duration-150 active:bg-transparent/50`}
             >
               <svg
-                className={cn(
-                  "w-5 h-5 group-hover:-translate-y-1 duration-300 transition-all ",
-                  reset ? "translate-y-0" : "tranistion-transform"
-                )}
-                viewBox="0 0 48 48"
-                fill="none"
                 xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M15.095 43.5014C33.2083 43.5014 43.1155 28.4946 43.1155 15.4809C43.1155 15.0546 43.1155 14.6303 43.0867 14.2079C45.0141 12.8138 46.6778 11.0877 48 9.11033C46.2028 9.90713 44.2961 10.4294 42.3437 10.6598C44.3996 9.42915 45.9383 7.49333 46.6733 5.21273C44.7402 6.35994 42.6253 7.16838 40.4198 7.60313C38.935 6.02428 36.9712 4.97881 34.8324 4.6285C32.6935 4.27818 30.4988 4.64256 28.5879 5.66523C26.677 6.68791 25.1564 8.31187 24.2615 10.2858C23.3665 12.2598 23.1471 14.4737 23.6371 16.5849C19.7218 16.3885 15.8915 15.371 12.3949 13.5983C8.89831 11.8257 5.81353 9.33765 3.3408 6.29561C2.08146 8.4636 1.69574 11.0301 2.2622 13.4725C2.82865 15.9148 4.30468 18.0495 6.38976 19.4418C4.82246 19.3959 3.2893 18.9731 1.92 18.2092V18.334C1.92062 20.6077 2.7077 22.8112 4.14774 24.5707C5.58778 26.3303 7.59212 27.5375 9.8208 27.9878C8.37096 28.3832 6.84975 28.441 5.37408 28.1567C6.00363 30.1134 7.22886 31.8244 8.87848 33.0506C10.5281 34.2768 12.5197 34.9569 14.5747 34.9958C12.5329 36.6007 10.1946 37.7873 7.69375 38.4878C5.19287 39.1882 2.57843 39.3886 0 39.0777C4.50367 41.9677 9.74385 43.5007 15.095 43.4937"
-                  fill="#1DA1F2"
-                />
-              </svg>
+                enable-background="new 0 0 72 72"
+                viewBox="0 0 72 72"
+                id="twitter-x"
+                fill="white"
+                className={cn(
+                    "w-5 h-5 group-hover:-translate-y-1 duration-300 transition-all ",
+                    reset
+                        ? "translate-y-0"
+                        : "tranistion-transform"
+                )}
+                >
+                    <switch>
+                        <g>
+                            <path
+                                d="M42.5,31.2L66,6h-6L39.8,27.6L24,6H4l24.6,33.6L4,66h6l21.3-22.8L48,66h20L42.5,31.2z M12.9,10h8l38.1,52h-8L12.9,10z"
+                            ></path>
+                        </g>
+                    </switch>
+                </svg>
             </button>
             <button
               onMouseEnter={() => setReset(false)}
