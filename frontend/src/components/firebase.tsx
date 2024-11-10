af06566be64744bbfa6200ef9d6859ad8ea9bdcb
Abhi-hertz <93651229+AE-Hertz@users.noreply.github.com>
2024-11-10 13:49:54 +0530

    feat: signin with multiple providers

---
+++ frontend/src/components/firebase.tsx
@@ -0,0 +1,34 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+
+// Define the type for the Firebase configuration
+interface FirebaseConfig {
+    apiKey: string;
+    authDomain: string;
+    projectId: string;
+    storageBucket: string;
+    messagingSenderId: string;
+    appId: string;
+}
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig: FirebaseConfig = {
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+};
+
+// Initialize Firebase if it hasn't been initialized yet
+const app = initializeApp(firebaseConfig);
+
+// Initialize Firebase
+export const auth = getAuth(app);
+export const googleProvider = new GoogleAuthProvider();
